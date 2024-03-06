5ce5a6ff3640905ed5d423effd3b08577e6854ef
Andreas Wagner <andreaswagner@mein.gmx>
2024-03-06 17:59:25 +0000

    added jooble.org post api request

--- src/api_test.py
+++ src/api_test.py
@@ -6,30 +6,50 @@ from urllib.parse import urlparse
 ADZUNA_APP_ID = "fb3d1b6c"
 ADZUNA_APP_KEY = "d1999430f1b272b9af611b798e8b0789"
 
+JOOBLE_API_KEY = "b056f9bf-136f-4fd5-8721-6fdc0d4453bc"
+
 page_number = 1
 
 header_list = [{
     "ACCEPT": "application/json",  
 }]
-url_list = [f"https://api.adzuna.com/v1/api/jobs/de/search/{page_number}?app_id={ADZUNA_APP_ID}&app_key={ADZUNA_APP_KEY}",
-            f"https://www.themuse.com/api/public/jobs?page={page_number}"]
 
-urls = zip(url_list, header_list *len(url_list) )
+urls = [(f"https://api.adzuna.com/v1/api/jobs/de/search/{page_number}?app_id={ADZUNA_APP_ID}&app_key={ADZUNA_APP_KEY}","GET", header_list[0], None),
+            (f"https://www.themuse.com/api/public/jobs?page={page_number}", "GET", header_list[0], None),
+            (f"http://jooble.org/api/{JOOBLE_API_KEY}", "POST", header_list[0], '{ "keywords": "it", "location": "Berlin"}')]
 
-for url, headers in urls:
 
-    print("REQUEST API FOR:",url, headers)
-    response = requests.get(url, headers=headers)
+print(list(urls))
+for url, connect_type, headers, body in urls:
 
+    print("REQUEST API FOR:",url,connect_type, headers)
+    if connect_type == "GET":
+        response = requests.get(url, headers=headers)
+        fname =  urlparse(url).hostname + f"--page{page_number}.json"
+    else:
+        response = requests.post(url, headers=headers, data=body)
+        fname =  urlparse(url).hostname + f"--data:{body}.json"
     if response.status_code == 200:
         data = response.text  
-        fname =  urlparse(url).hostname + f"--page{page_number}.json"
         path_src = os.path.join(os.path.dirname(__file__))
         path_project = path_src.split("src")[0] 
         path_data = path_project + "data/raw/"
-        print(path_data)
         with open(f"{path_data}{fname}", 'w') as file:
             file.write(data)
     else:
         print('ERROR:', response.status_code)
     
+
+
+
+"""
+connection = http.client.HTTPConnection(host)
+#request headers
+headers = {"Content-type": "application/json"}
+#json query
+body = '{ "keywords": "it", "location": "Bern"}'
+connection.request('POST','/api/' + key, body, headers)
+response = connection.getresponse()
+print(response.status, response.reason)
+print(response.read())
+"""
