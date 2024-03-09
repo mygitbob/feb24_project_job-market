e7f99235a127aeb1cac393b6ffe73c2af5e17817
Andreas Wagner <andreaswagner@mein.gmx>
2024-03-09 11:33:52 +0000

    fixed logger

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,7 @@
 import logging
+import sys
 
-def setup_logging(log_file):
-    if log_file != 'stdout':
+def setup_logging(log_file=sys.stdout, level=logging.DEBUG):
+    if log_file != sys.stdout:
         with open(log_file, 'w'): pass # reset log
     logging.basicConfig(filename=log_file, level=logging.DEBUG)
