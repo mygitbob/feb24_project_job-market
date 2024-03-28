120392bed46e5bce666ad0d7e59c9159f9f2fa76
Yue <yuewang9515@gmail.com>
2024-03-28 00:08:27 +0000

    update

--- src/pymongo.py
+++ src/pymongo.py
@@ -2,31 +2,37 @@ import pymongo
 import json
 
 # connect to MongoDB
-client = pymongo.MongoClient(
-    host="127.0.0.1",
-    port = 27017
-    )
-db = client["jobmarket"]
+def connect_to_mongo(host, port, db_name):
+    client = pymongo.MongoClient(host=host, port=port)
+    db = client[db_name]
+    return db
+def read_json_file(file_path):
+    with open(file_path, "r") as file:
+        data = json.load(file)
+    return data
 
-# create collections variables for two datasource
-okjobs_collection = db["okjobs"]
-reed_collection = db["reed"]
+def insert_data_into_collection(collection, data):
+    for item in data:
+        if not collection.find_one({"id": item["id"]}):
+            collection.insert_one(item)
 
-#read json files
-with open("/data/processed/row_sample_data/okjob_raw.json", "r") as okjobs_file:
-    okjobs_data = json.load(okjobs_file)
+def main():
+    # Connect to MongoDB
+    db = connect_to_mongo("127.0.0.1", 27017, "jobmarket")
 
-with open("/data/processed/row_sample_data/reed_raw.json", "r") as reed_file:
-    reed_data = json.load(reed_file)
+    # Create collections variables for two data sources
+    okjobs_collection = db["okjobs"]
+    reed_collection = db["reed"]
 
-# insert data into collections using unique id
+    # Read JSON files
+    okjobs_data = read_json_file("/data/processed/row_sample_data/okjob_raw.json")
+    reed_data = read_json_file("/data/processed/row_sample_data/reed_raw.json")
 
-for job in okjobs_data:
-    if not okjobs_collection.find_one({"id": job["id"]}):
-        okjobs_collection.insert_one(job)
+    # Insert data into collections using unique id
+    insert_data_into_collection(okjobs_collection, okjobs_data)
+    insert_data_into_collection(reed_collection, reed_data)
 
-for job in reed_data:
-    if not reed_collection.find_one({"id": job["id"]}):
-        reed_collection.insert_one(job)
+if __name__ == "__main__":
+    main()
 
 
