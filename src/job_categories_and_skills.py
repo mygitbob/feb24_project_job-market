c98fce59eaebdde5ba28ef9355619d8351196031
Boris Tsonkov <birkata@yahoo.com>
2024-04-20 21:44:37 +0100

    added categories and skills configuration file

---
+++ src/job_categories_and_skills.py
@@ -0,0 +1,30 @@
+keywords_title = {
+    "data administrator": ["data", "administrator", "entry", "protection", "officer", "clerk", "admin", "migration",
+                           "cleanser", "inputter", "coordinator", "assistant", "pocessor", "auditor", "governance",
+                           "apprentice", "executive", "manager"],
+    "data engineer": ["data", "engineer", "developer", "engineering", "modeller", "technical"],
+    "data analyst": ["data", "analyst", "analytics", "analysis", "investigation", "workstream", "visualisation",
+                     "insight", "consultant"],
+    "database administrator": ["database", "administrator", "assistant", "manager"],
+    "data scientist": ["data", "scientist", "science", "engineer"],
+    "data center": ["data", "center", "cabling", "installer", "installation", "engineer"],
+    "data test": ["data", "test", "tester", "automation", "processing"],
+    "data architect": ["data", "architect"],
+    "manager": ["head", "manager", "director", "procurement", "management"]
+}
+
+keywords_skills = [
+    "SQL", "Structured Query Language", "Python", "R", "Docker", "AWS", "Amazon Web Services",
+    "Azure", "Google Cloud Platform", "GCP", "Snowflake", "Hadoop", "Spark", "Kubernetes",
+    "Jenkins", "BI", "Business Intelligence", "Tableau", "Power BI", "Looker", "ETL",
+    "Extract Transform Load", "Informatica", "Talend", "SSIS", "CRM",
+    "Customer Relationship Management", "Salesforce", "SAP", "Git", "NoSQL", "MongoDB",
+    "Cassandra", "PostgreSQL", "MySQL", "Data Modeling", "Machine Learning", "ML", "AI",
+    "Apache Kafka", "Redis", "Elasticsearch", "Kibana", "Ansible", "REST", "RESTful", "API",
+    "GraphQL", "Linux", "Matplotlib", "Seaborn", "Jupyter Notebook", "Scikit-learn",
+    "TensorFlow", "PyTorch", "Data Lakes", "Data Warehousing", "Agile", "Scrum", "Blockchain",
+    "Edge Computing", "VMware", "SAS", "Flask", "Django", "Apache", "Airflow", "Luigi", "NLP",
+    "Databricks", "redshift", "Excel", "HANA", "Oracle", "crypto"
+]
+
+keywords_site = ['remote', 'hybrid', 'on-site']
