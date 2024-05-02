391fd8283e7000f917766c143292f57ee1d374df
mrburns <a.wagner@bbqberlin.onmicrosoft.com>
2024-05-02 18:05:38 +0200

    fixed flag order

--- restart_services.sh
+++ restart_services.sh
@@ -8,14 +8,14 @@ export PIPELINE_ACTION="not used here"
 
 if ! docker-compose ps | grep -q 'jobmarket_db'; then
     echo "Restarting postgres container..."
-    docker-compose -d up jobmarket_db
+    docker-compose up -d jobmarket_db
 else
     echo "Postgres container is running."
 fi
 
 if ! docker-compose ps | grep -q 'jobmarket_api'; then
     echo "Restarting api container..."
-    docker-compose -d up jobmarket_api
+    docker-compose up -d jobmarket_api
 else
     echo "Api container is running."
 fi
