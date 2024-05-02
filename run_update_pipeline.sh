d211c09fa4f0d9c587f0986748ae573639f05afc
mrburns <a.wagner@bbqberlin.onmicrosoft.com>
2024-05-02 18:35:54 +0200

    added delete old updates

--- run_update_pipeline.sh
+++ run_update_pipeline.sh
@@ -3,18 +3,20 @@
 # check if database and api are still running and restart them if not
 bash ./restart_services.sh 
 
-# lead environment variables
+# load environment variables
 source .env
 # set PIPELINE_ACTION for update phase
 export PIPELINE_ACTION="update"
 
-# start data retrieval update process
+# delete old data if anything is left
+rm -rf ./data/processed/muse.com/merged/* 2>/dev/null
+rm -rf ./data/processed/okjob.io/merged/* 2>/dev/null
+rm -rf ./data/processed/reed.co.uk/merged/* 2>/dev/null
+
 docker-compose up -d jobmarket_db jobmarket_data_retrieval
 
-# warte auf Abschluss der Datenabrufaktualisierung und starte dann die Transformation
 docker wait jobmarket_data_retrieval_container
 docker-compose up -d jobmarket_transform 
 
-# warte auf Abschluss der Transformation und starte dann die Modellerstellung
 docker wait jobmarket_transform_container
 docker-compose up -d jobmarket_model
